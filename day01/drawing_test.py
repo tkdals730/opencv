4094c49969cec13b3d3f29dcebfdbfa1c01bf112
Sangmin <tkdals730@naver.com>
2026-03-30 14:33:23 +0900

    타원색상 변경

--- day01/drawing_test.py
+++ day01/drawing_test.py
@@ -15,15 +15,15 @@ img = np.zeros((512,512,3), np.uint8)
 # BGR
 cv.line(img,(0,0),(511,511),(0, 255, 255),2)
 # 네모
-cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
+cv.rectangle(img,(384,10),(510,138),(0,255,0),3)
 # 동그라미
-cv.circle(img,(447,63), 63, (0,0,255), -1)
+cv.circle(img,(447,74), 63, (0,0,255), -1)
 # 타원
-cv.ellipse(img,(256,256),(150,80),0,0,180,255,-1)
+cv.ellipse(img,(256,256),(150,80),0,0,180,(255,255,255),-1)
 # 폴리건????
 pts = np.array([[250,15],[30,40],[70,20]], np.int32)
 pts = pts.reshape((-1,1,2))
-cv.polylines(img,[pts],True,(0,255,0))
+cv.polylines(img,[pts],True,(0,255,0),5)
 
 font = cv.FONT_HERSHEY_SIMPLEX
 cv.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv.LINE_AA)
