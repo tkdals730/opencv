7f07aebe7f3f72c79f81c43bf9b7cfc20e9004c0
Sangmin <tkdals730@naver.com>
2026-03-30 16:09:38 +0900

    속이 빈 사각형생성

--- day01/mouse_rectangle2.py
+++ day01/mouse_rectangle2.py
@@ -1,42 +1,47 @@
 import numpy as np
 import cv2 as cv
- 
-drawing = False # true if mouse is pressed
-mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-ix,iy = -1,-1
- 
-# mouse callback function
-def draw_circle(event,x,y,flags,param):
-    global ix,iy,drawing,mode
- 
+
+drawing = False
+mode = True
+ix, iy = -1, -1
+
+img = np.zeros((512, 512, 3), np.uint8)
+temp_img = img.copy()
+
+def draw_shape(event, x, y, flags, param):
+    global ix, iy, drawing, mode, img, temp_img
+
     if event == cv.EVENT_LBUTTONDOWN:
         drawing = True
-        ix,iy = x,y
- 
+        ix, iy = x, y
+
     elif event == cv.EVENT_MOUSEMOVE:
-        if drawing == True:
-            if mode == True:
-                cv.rectangle(img,(ix,iy),(x,y),(0,255,0), 1)
+        if drawing:
+            temp_img = img.copy()   # 원본 복사
+            if mode:
+                cv.rectangle(temp_img, (ix, iy), (x, y), (0, 255, 0), 2)
             else:
-                cv.circle(img,(x,y),5,(0,0,255),-1)
- 
+                cv.circle(temp_img, (x, y), 5, (0, 0, 255), -1)
+
     elif event == cv.EVENT_LBUTTONUP:
         drawing = False
-        if mode == True:
-            cv.rectangle(img,(ix,iy),(x,y),(0,255,0),0)
+        if mode:
+            cv.rectangle(img, (ix, iy), (x, y), (0, 255, 0), 2)
         else:
-            cv.circle(img,(x,y),5,(0,0,255),-1)
+            cv.circle(img, (x, y), 5, (0, 0, 255), -1)
 
-img = np.zeros((512,512,3), np.uint8)
+        temp_img = img.copy()
+
 cv.namedWindow('image')
-cv.setMouseCallback('image',draw_circle)
- 
-while(1):
-    cv.imshow('image',img)
+cv.setMouseCallback('image', draw_shape)
+
+while True:
+    cv.imshow('image', temp_img)
     k = cv.waitKey(1) & 0xFF
+
     if k == ord('m'):
         mode = not mode
     elif k == 27:
         break
- 
+
 cv.destroyAllWindows()
