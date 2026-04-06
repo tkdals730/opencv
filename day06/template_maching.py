673f09da84f2a267aadd629adc1bbd4bc34317c6
Sangmin <tkdals730@naver.com>
2026-04-06 11:07:15 +0900

    템플릿매칭 실습코드 및 이미지 다운

---
+++ day06/template_maching.py
@@ -0,0 +1,59 @@
+import urllib.request
+import os
+import cv2 as cv 
+from matplotlib import pyplot as plt 
+
+def get_sample(filename, repo='opencv'):
+    """부교 또는 OpenCV 공식 샘플 이미지 자동 다운로드
+    
+    Args:
+        filename (str): 이미지 파일명 (예: 'morphological.png')
+        repo (str): 'insightbook' (부교) 또는 'opencv' (공식)
+    
+    Returns:
+        str: 다운로드된 파일명
+    """
+    if not os.path.exists(filename):
+        if repo == 'insightbook':
+            url = f"https://raw.githubusercontent.com/dltpdn/insightbook.opencv_project_python/master/img/{filename}"
+        else:  # opencv 공식
+            url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
+        urllib.request.urlretrieve(url, filename)
+    return filename
+
+# 사용 방법
+# img = cv.imread(get_sample('morphological.png', repo='insightbook'))
+
+img = cv.imread(get_sample('messi5.jpg'), cv.IMREAD_GRAYSCALE)
+img2 = img.copy()
+template = cv.imread('template.jpg', cv.IMREAD_GRAYSCALE)
+w, h = template.shape[::-1]
+
+# All the 6 methods for comparison in a list
+methods = ['TM_CCOEFF', 'TM_CCOEFF_NORMED', 'TM_CCORR',
+            'TM_CCORR_NORMED', 'TM_SQDIFF', 'TM_SQDIFF_NORMED']
+
+for meth in methods:
+    img = img2.copy()
+    method = getattr(cv, meth)
+ 
+    # Apply template Matching
+    res = cv.matchTemplate(img,template,method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+ 
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+ 
+    cv.rectangle(img,top_left, bottom_right, 255, 2)
+ 
+    plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(img,cmap = 'gray')
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.suptitle(meth)
+ 
+    plt.show()
--- image_download.py
+++ image_download.py
@@ -1,12 +1,24 @@
 import urllib.request
 import os
-import cv2
+import cv2 as cv
 
-def download_sample(filename):
+def get_sample(filename, repo='insightbook'):
+    """부교 또는 OpenCV 공식 샘플 이미지 자동 다운로드
+    
+    Args:
+        filename (str): 이미지 파일명 (예: 'morphological.png')
+        repo (str): 'insightbook' (부교) 또는 'opencv' (공식)
+    
+    Returns:
+        str: 다운로드된 파일명
+    """
     if not os.path.exists(filename):
-        url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
+        if repo == 'insightbook':
+            url = f"https://raw.githubusercontent.com/dltpdn/insightbook.opencv_project_python/master/img/{filename}"
+        else:  # opencv 공식
+            url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
         urllib.request.urlretrieve(url, filename)
     return filename
 
-# 사용할 샘플 이미지
-img = cv2.imread(download_sample("smarties.png"), cv2.IMREAD_GRAYSCALE)
+# 사용 방법
+img = cv.imread(get_sample('mario.png', repo='opencv'))
